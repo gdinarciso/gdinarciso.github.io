414382b88b22692e8f68e9e26b24fc182128c76e
na <na@inac.modem>
2020-07-31 16:39:21 +1000

    2th daily update

--- personalsolution/script.js
+++ personalsolution/script.js
@@ -1,19 +1,19 @@
 function openPage(pageName,elmnt,color) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("tabcontentback");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "rgb(50,50,50)";
   }
   document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
+  elmnt.style.color = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("Open").click();
 
 //____________________________________________________________
 
