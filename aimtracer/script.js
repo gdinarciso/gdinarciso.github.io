5a4bc207d03438d45c40e58ec203e8fa960cc3ca
na <na@inac.modem>
2020-08-06 21:08:25 +1000

    daily update

--- aimtracer/script.js
+++ aimtracer/script.js
@@ -1,14 +1,27 @@
 /* Open the side */
 function openHardware() {document.getElementById("hardware").style.width = "100%";}
-function openSoftware() {document.getElementById("software").style.width = "100%";}
-function openNetwork() {document.getElementById("network").style.width = "100%";}
-
 function openHardware1() {document.getElementById("hardware1").style.width = "100%";}
+function openHardware2() {document.getElementById("hardware2").style.width = "100%";}
 
+function openSoftware() {document.getElementById("software").style.width = "100%";}
+function openSoftware1() {document.getElementById("software1").style.width = "100%";}
+function openSoftware2() {document.getElementById("software2").style.width = "100%";}
+function openSoftware3() {document.getElementById("software3").style.width = "100%";}
+
+function openNetwork() {document.getElementById("network").style.width = "100%";}
+function openNetwork1() {document.getElementById("network1").style.width = "100%";}
+function openNetwork2() {document.getElementById("network2").style.width = "100%";}
 
 /* Close/hide the side */
 function closeHardware() {document.getElementById("hardware").style.width = "0";}
-function closeSoftware() {document.getElementById("software").style.width = "0";}
-function closeNetwork() {document.getElementById("network").style.width = "0";}
-
 function closeHardware1() {document.getElementById("hardware1").style.width = "0%";}
+function closeHardware2() {document.getElementById("hardware2").style.width = "0%";}
+
+function closeSoftware() {document.getElementById("software").style.width = "0";}
+function closeSoftware1() {document.getElementById("software1").style.width = "0";}
+function closeSoftware2() {document.getElementById("software2").style.width = "0";}
+function closeSoftware3() {document.getElementById("software3").style.width = "0";}
+
+function closeNetwork() {document.getElementById("network").style.width = "0";}
+function closeNetwork1() {document.getElementById("network1").style.width = "0";}
+function closeNetwork2() {document.getElementById("network2").style.width = "0";}
