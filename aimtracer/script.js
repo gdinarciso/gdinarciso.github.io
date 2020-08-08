32aaddc97534721adaa80bb92e5f0d0057eee39d
Narciso <na@Narcisos-MBP.modem>
2020-08-08 13:41:44 +1000

    daily update

--- aimtracer/script.js
+++ aimtracer/script.js
@@ -1,27 +1,32 @@
 /* Open the side */
-function openHardware() {document.getElementById("hardware").style.width = "100%";}
+function currentHeight() {
+    var x = "Total Height: " + screen.height + "px";
+    document.getElementById("aimtracer").innerHTML = x;
+}
+
+function openHardware() {document.getElementById("hardware").style.height = "100%";}
 function openHardware1() {document.getElementById("hardware1").style.width = "100%";}
 function openHardware2() {document.getElementById("hardware2").style.width = "100%";}
 
-function openSoftware() {document.getElementById("software").style.width = "100%";}
+function openSoftware() {document.getElementById("software").style.height = "100%";}
 function openSoftware1() {document.getElementById("software1").style.width = "100%";}
 function openSoftware2() {document.getElementById("software2").style.width = "100%";}
 function openSoftware3() {document.getElementById("software3").style.width = "100%";}
 
-function openNetwork() {document.getElementById("network").style.width = "100%";}
+function openNetwork() {document.getElementById("network").style.height = "100%";}
 function openNetwork1() {document.getElementById("network1").style.width = "100%";}
 function openNetwork2() {document.getElementById("network2").style.width = "100%";}
 
 /* Close/hide the side */
-function closeHardware() {document.getElementById("hardware").style.width = "0";}
+function closeHardware() {document.getElementById("hardware").style.height = "0";}
 function closeHardware1() {document.getElementById("hardware1").style.width = "0%";}
 function closeHardware2() {document.getElementById("hardware2").style.width = "0%";}
 
-function closeSoftware() {document.getElementById("software").style.width = "0";}
+function closeSoftware() {document.getElementById("software").style.height = "0";}
 function closeSoftware1() {document.getElementById("software1").style.width = "0";}
 function closeSoftware2() {document.getElementById("software2").style.width = "0";}
 function closeSoftware3() {document.getElementById("software3").style.width = "0";}
 
-function closeNetwork() {document.getElementById("network").style.width = "0";}
+function closeNetwork() {document.getElementById("network").style.height = "0";}
 function closeNetwork1() {document.getElementById("network1").style.width = "0";}
 function closeNetwork2() {document.getElementById("network2").style.width = "0";}
