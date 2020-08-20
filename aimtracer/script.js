01d3c81608160fed41d0f13f6bb0b96b5979b722
na <na@inac.modem>
2020-08-20 20:15:00 +1000

    add game mobile

--- aimtracer/script.js
+++ aimtracer/script.js
@@ -4,28 +4,41 @@ function currentHeight() {
     document.getElementById("aimtracer").innerHTML = x;
 }
 
-function openHardware() {document.getElementById("hardware").style.height = "100%";}
-function openHardware1() {document.getElementById("hardware1").style.width = "100%";}
-function openHardware2() {document.getElementById("hardware2").style.width = "100%";}
-
 function openSoftware() {document.getElementById("software").style.height = "100%";}
 function openSoftware1() {document.getElementById("software1").style.width = "100%";}
 function openSoftware2() {document.getElementById("software2").style.width = "100%";}
 function openSoftware3() {document.getElementById("software3").style.width = "100%";}
+
+function openGame() {document.getElementById("game").style.height = "100%";}
+function openGame1() {document.getElementById("game1").style.width = "100%";}
+
+function openMobile() {document.getElementById("mobile").style.height = "100%";}
+function openMobile1() {document.getElementById("mobile1").style.width = "100%";}
+
+function openHardware() {document.getElementById("hardware").style.height = "100%";}
+function openHardware1() {document.getElementById("hardware1").style.width = "100%";}
+function openHardware2() {document.getElementById("hardware2").style.width = "100%";}
 
 function openNetwork() {document.getElementById("network").style.height = "100%";}
 function openNetwork1() {document.getElementById("network1").style.width = "100%";}
 function openNetwork2() {document.getElementById("network2").style.width = "100%";}
 
 /* Close/hide the side */
-function closeHardware() {document.getElementById("hardware").style.height = "0";}
-function closeHardware1() {document.getElementById("hardware1").style.width = "0%";}
-function closeHardware2() {document.getElementById("hardware2").style.width = "0%";}
 
 function closeSoftware() {document.getElementById("software").style.height = "0";}
 function closeSoftware1() {document.getElementById("software1").style.width = "0";}
 function closeSoftware2() {document.getElementById("software2").style.width = "0";}
 function closeSoftware3() {document.getElementById("software3").style.width = "0";}
+
+function closeGame() {document.getElementById("game").style.height = "0";}
+function closeGame1() {document.getElementById("game1").style.width = "0";}
+
+function closeMobile() {document.getElementById("mobile").style.height = "0";}
+function closeMobile1() {document.getElementById("mobile1").style.width = "0";}
+
+function closeHardware() {document.getElementById("hardware").style.height = "0";}
+function closeHardware1() {document.getElementById("hardware1").style.width = "0%";}
+function closeHardware2() {document.getElementById("hardware2").style.width = "0%";}
 
 function closeNetwork() {document.getElementById("network").style.height = "0";}
 function closeNetwork1() {document.getElementById("network1").style.width = "0";}
