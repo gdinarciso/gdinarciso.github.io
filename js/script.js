1cc430f159e103c064d5dbb208a9513bc06f1669
gdinarciso <gdinarciso@gmail.com>
2021-08-04 12:21:28 +1000

    4/8/21 update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let darkMode = localStorage.getItem('darkMode');
-if (darkMode === 'on') {
+let darkMode = localStorage.getItem("darkMode");
+if (darkMode === "on") {
   darkModeOn();
 }
 
 function darkModeOn() {
-  document.getElementById('darkMode').href = '/css/dark.css';
-  localStorage.setItem('darkMode', 'on');
+  document.getElementById("darkMode").href = "/css/dark.css";
+  localStorage.setItem("darkMode", "on");
 }
 
 function darkModeOff() {
-  document.getElementById('darkMode').href = '';
-  localStorage.setItem('darkMode', null);
+  document.getElementById("darkMode").href = "";
+  localStorage.setItem("darkMode", null);
 }
 
-function darkSwitch () {
-  darkMode = localStorage.getItem('darkMode');
-  if (darkMode !== 'on') {
+function darkSwitch() {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "on") {
     darkModeOn();
-} else {
+  } else {
     darkModeOff();
   }
 }
@@ -39,27 +39,31 @@ function includeHTML() {
   let file;
   let xhttp;
   /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName('*');
+  z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute('include-html');
+    file = elmnt.getAttribute("include-html");
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
 
-          if (this.status == 404) {elmnt.innerHTML = 'Page not found.';}
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
 
           /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute('include-html');
+          elmnt.removeAttribute("include-html");
           includeHTML();
         }
       };
 
-      xhttp.open('GET', file, true);
+      xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
       return;
@@ -110,7 +114,7 @@ function includeHTML() {
 //                   <a href=""><i class="fab fa-facebook"></i></a>
 //                   <a href="mailto:narciso.sbgr@gmail.com"><i class="fas fa-envelope"></i></a>
 //               </section>
-          
+
 //               <p class="rights"><i class="far fa-copyright"></i> 2021. Made with <i class="fa fa-heart" title="LOVE"></i> and <i class="fa fa-brain" title="DEEP WORK"></i> in Sydney, Australia. Narciso Sbrissa Grimaldi.</p>
 //           </footer>
 //       `;
