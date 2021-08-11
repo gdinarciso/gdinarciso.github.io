9a9b7270887d5b43dbcaef564cf5b8d3a3d72213
gdinarciso <gdinarciso@gmail.com>
2021-08-11 16:43:26 +1000

    11/8/21 Daily update

--- js/script.js
+++ js/script.js
@@ -76,39 +76,17 @@ function includeHTML() {
   }
 }
 
-// See https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
-// class Header extends HTMLElement {
-//   constructor() {
-//     super();
-//   }
-//   connectedCallback() {
-//     this.innerHTML = `
-//       <header>
-//         <i class="moonsunicon fas fa-moon" onclick="darkSwitch()" title='Swicth to dark mode'></i>
-//         <i class="moonsunicon fas fa-sun" onclick="darkSwitch()" title='Swicth to light mode'></i>
-//         <!-- <button  onclick="darkSwitch()" title='Swicth to dark mode'></button> -->
-
-//         <h1 class="greeting">Hello and welcome aboard!</h1>
-//         <!-- <p class="item phrase">Deep work is the way to your purpose. Keep working!</p> -->
-
-//         <nav id="header-nav">
-//           <div class="borderAni"><a href="/index.html"><i class="fas fa-igloo"></i>HOME</a></div>
-//           <div class="borderAni"><a href="/html/it.html"><i class="fas fa-microchip"></i>I.T.</a></div>
-//           <div class="borderAni"><a href="/html/music.html"><i class="fas fa-drum"></i>MUSIC</a></div>
-//           <div class="borderAni"><a href="/html/resume.html"><i class="fas fa-file"></i>RÉSUMÉ</a></div>
-//         </nav>
-//       </header>
-//     `;
-//   }
-// }
-// customElements.define("my-header", Header);
-
+const navContainer = document.getElementById("header-nav");
 // let btnContainer = document.getElementById("header-nav");
-// let btns = btnContainer.getElementsByClassName("btn");
-// for (let i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
+const abtns = navContainer.getElementsByClassName("abtn");
+// let abtns = navContainer.getElementsByClassName("abtn");
+for (let i = 0; i < abtns.length; i++) {
+  abtns[i].addEventListener("click", function () {
+    const current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
