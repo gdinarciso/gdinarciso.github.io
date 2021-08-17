385d1d96d02919ffb670b842de4df95c3861dd8d
gdinarciso <gdinarciso@gmail.com>
2021-08-17 15:52:19 +1000

    17/8/21 Daily update

--- js/counter.js
+++ js/counter.js
@@ -1,35 +1,35 @@
 function hello() {
-    document.querySelector('h1').innerHTML = 'Hellyeah!';
+  document.querySelector("h2").innerHTML = "Hellyeah!";
 }
 /* first example
 let counter = 0; */
 
 //part of 3th example
-if (!localStorage.getItem('counter')) {
-    localStorage.setItem('counter', 0);
+if (!localStorage.getItem("counter")) {
+  localStorage.setItem("counter", 0);
 }
 
 function count() {
-    //part of 3th example
-    let counter = localStorage.getItem('counter');
+  //part of 3th example
+  let counter = localStorage.getItem("counter");
 
-    counter++;
-    document.querySelector('h1').innerHTML = counter;
-    localStorage.setItem('counter', counter);
+  counter++;
+  document.querySelector("h2").innerHTML = counter;
+  localStorage.setItem("counter", counter);
 
-    /* first example
+  /* first example
     if (counter % 10 === 0) {
         alert(`Counter is now ${counter}`);
     }
     */
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    //part of 3th example
-    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+document.addEventListener("DOMContentLoaded", function () {
+  //part of 3th example
+  document.querySelector("h2").innerHTML = localStorage.getItem("counter");
 
-    document.querySelector('button').onclick = count;
+  document.querySelector("button").onclick = count;
 
-    /* part of 2th example
+  /* part of 2th example
     setInterval(count, 1000); */
 });
