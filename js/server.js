c0ffdb3d4017ff56caa37ecc0098f1e35a14510b
gdinarciso <gdinarciso@gmail.com>
2021-08-21 11:15:48 +1000

    21/8/21 Node.js

---
+++ js/server.js
@@ -0,0 +1,28 @@
+const port = 5600;
+
+// const http = require("http");
+
+// const server = http.createServer(function (req, res) {
+//   console.log(`user visited ${req.url}`);
+//   res.end("Hello! Here is Narciso. Welcome to my Node.js Server");
+// });
+
+// server.listen(port);
+// console.log("listening on http://localhost:" + port);
+// // { server.listen(3000); console.log("listening on http://localhost:3000");}
+
+const express = require("express");
+
+const app = express(); //stands for one server
+
+app.get("/", function (req, res) {
+  res.end("Welcome to my site!");
+});
+
+app.get("/complement", function (req, res) {
+  res.end("You look nice today");
+});
+
+app.listen(port);
+console.log("listening on http://localhost:" + port);
+// console.log("listening on http://localhost:3000");
